f5ee759c8cf8b4328f4f7d6b356740944e218bb3
kamrul25 <kamrul.coder01@gmail.com>
2023-03-03 05:03:09 +0600

    card debugging

--- js/ai-hub.js
+++ js/ai-hub.js
@@ -16,19 +16,19 @@ const displayAI = (tools) => {
     // console.log(tool);
     const { name, image, published_in, features } = tool;
     const createDiv = document.createElement("div");
-    createDiv.classList.add("col");
+    createDiv.classList.add("col", "mb-3");
     createDiv.innerHTML = `         
-        <div class="card p-4">
-          <img src=${image} class="card-img-top img-fluid rounded" alt="">
+        <div class="card p-3 ">
+          <img src=${image} class="card-img-top img-fluid image " alt="">
           <div class="">
             <h3 class="card-title mt-4 mb-3">Features</h3>
-            <ol class="card-text">
-              <li class="card-text">${features[0]}</li>
-              <li class="card-text">${features[1]}</li>
-              <li class="card-text">${features[2] ? features[2] : ""}</li>
-            </ol>
+            <div class="card-text">
+              <p >1. ${features[0]}</p>
+              <p >2. ${features[1]}</p>
+              <p >3. ${features[2] ? features[2] : ""}</p>
+            </div>
           </div>
-          <hr class="mt-4">
+          <hr class="">
           <div class="d-flex justify-content-between align-items-center">
             <div>
               <h3 class="card-title" >${name}</h3>
