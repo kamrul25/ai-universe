157b31387cab38b6794b01434d2f90535745f35d
kamrul25 <kamrul.coder01@gmail.com>
2023-03-03 03:20:05 +0600

    give error massage

--- js/ai-hub.js
+++ js/ai-hub.js
@@ -1,6 +1,13 @@
 // Load AI Universe Hub API by fetch
 const fetchAI = () =>{
     const url = `https://openapi.programming-hero.com/api/ai/tools`;
-    fetch(url).then(res => res.json()).then(data => console.log(data))
+    fetch(url).then(res => res.json()).then(data => displayAI(data.data.tools))
+    .catch((error) => {
+        console.error( error);
+    });
+}
+
+const displayAI = tools =>{
+    console.log(tools);
 }
 fetchAI();
