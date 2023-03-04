e2f942d12aff5860e39d656db63bda21e045c4a1
kamrul25 <kamrul.coder01@gmail.com>
2023-03-04 06:03:55 +0600

    half dynamic modal added

--- js/ai-hub.js
+++ js/ai-hub.js
@@ -1,35 +1,32 @@
 // Load AI Universe Hub API by fetch
-const fetchAI = (dataLimit) => {
+const fetchAi = (dataLimit) => {
   const url = `https://openapi.programming-hero.com/api/ai/tools`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => displayAI(data.data.tools, dataLimit))
+    .then((data) => displayAi(data.data.tools, dataLimit))
     .catch((error) => {
       console.error(error);
     });
 };
 
-const displayAI = (tools , dataLimit) => {
+const displayAi = (tools, dataLimit) => {
   const toolsContainer = document.getElementById("tools-container");
   toolsContainer.innerHTML = "";
 
-   // display all ai by date to date when "sort by date "button clicked
+  // display all ai by date to date when "sort by date "button clicked
 
   // display all ai when show all button clicked
   const showAll = document.getElementById("show-all");
-  if(dataLimit && tools.length > 6){
-    tools = tools.slice(0,6);
+  if (dataLimit && tools.length > 6) {
+    tools = tools.slice(0, 6);
     showAll.classList.remove("d-none");
-  }
-  else{
+  } else {
     showAll.classList.add("d-none");
   }
 
   tools.forEach((tool) => {
     // console.log(tool);
-    const { name, image, published_in, features } = tool;
-
- 
+    const { id, name, image, published_in, features } = tool;
 
     const createDiv = document.createElement("div");
     createDiv.classList.add("col", "mb-3");
@@ -50,36 +47,86 @@ const displayAI = (tools , dataLimit) => {
               <h3 class="card-title" >${name}</h3>
               <i class="fa-regular fa-calendar-days "></i>  <span>${published_in}</span>
             </div>
-           
-            <i class="fa-solid fa-circle-arrow-right fa-2x text-danger cursor" data-bs-toggle="modal" data-bs-target="#exampleModal"></i>
-          
+            <i class="fa-solid fa-circle-arrow-right fa-2x text-danger cursor" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="fetchAiDetails('${id}')"></i>
           </div>
         </div>
     `;
     toolsContainer.appendChild(createDiv);
   });
-//  toggleSpinner(false);
+  //  toggleSpinner(false);
 };
 
 // Make serial by date
 
 // Showing all tools when seen more clicked
-document.getElementById("btn-show-all").addEventListener('click', function(){
-  fetchAI();
+document.getElementById("btn-show-all").addEventListener("click", function () {
+  fetchAi();
   // toggleSpinner(false);
 });
 
+// load data by id for modal
+const fetchAiDetails = (id) => {
+  // console.log(id)
+  const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayAiDetails(data.data))
+    .catch((error) => console.log(error));
+};
+// Display details about ai tool
+const displayAiDetails = (data) => {
+  const { image_link, input_output_examples, accuracy } = data;
+
+  const modalDetails = document.getElementById("modal-details");
+  modalDetails.innerHTML = `
+<div class="col ">
+  <div class="card">
+    <div class="card-body" onload="">
+        <img src=${image_link[0]} alt="" class="card-img-top img-fluid ">
+        <button id="accuracy-btn" onload="accuracyButton()" class="btn btn-danger position-accuracy "></button>
+        <div id="examples" class="text-center mt-4"></div>
+    </div>
+  </div>
+</div>
+  `;
+  const accuracyButton =()=>{
+    const accuracyDetail = document.getElementById("accuracy-btn");
+    const examples = document.getElementById("examples");
+    if(accuracy.score){
+      accuracyDetail.innerHTML =`${accuracy.score} accuracy`;
+      if(input_output_examples){
+        examples.innerHTML =`
+        <h5 class="h5">${input_output_examples[0].input}</h5>
+        <p class="card-title">${input_output_examples[0].output}</p>
+        `;
+      }else{
+        examples.innerHTML ="";
+      }
+    }
+    else{
+      accuracyDetail.classList.add("d-none");
+      if(input_output_examples){
+        examples.innerHTML =`
+        <h5 class="h5">${input_output_examples[1].input}</h5>
+        <p class="card-title">${input_output_examples[1].output}</p>
+        `;
+      }else{
+        examples.innerHTML ="";
+      }
+    }
+  }  
+  accuracyButton()
+};
+
 // Spinner
-const toggleSpinner = isLoading =>{
+const toggleSpinner = (isLoading) => {
   const loaderSection = document.getElementById("loader");
-  if(isLoading){
+  if (isLoading) {
     loaderSection.classList.remove("d-none");
-    fetchAI(7);
+    fetchAi(7);
     loaderSection.classList.add("d-none");
-  }else{
+  } else {
     loaderSection.classList.remove("d-none");
   }
 };
-
-toggleSpinner(true) ;
-
+toggleSpinner(true);
